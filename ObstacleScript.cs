17affc8b34aafe90ccd6ec8b5e331efc8458c813
aarnika <30500065+aarnika@users.noreply.github.com>
2017-07-30 14:32:58 -0500

    Create ObstacleScript.cs

---
+++ ObstacleScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour {
+	//speed of the obstacle object
+	public float objectSpeed = -0.5f;
+
+	void Update () {
+		if (Time.timeScale == 1) {
+			//moves the transform in the distance and direction of the translation 
+			transform.Translate (0, objectSpeed, 0);
+		}
+	}
+}
