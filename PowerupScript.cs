e6a2f8f6ca7e3d3b9dfe43e2323a45b65e1f9087
aarnika <30500065+aarnika@users.noreply.github.com>
2017-07-30 14:34:28 -0500

    Create PowerupScript.cs

---
+++ PowerupScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour {
+	//speed of the powerup object
+	public float objectSpeed = -0.5f;
+
+	void Update () {
+		if (Time.timeScale == 1) {
+			//moves the transform in the distance and direction of the translation
+			transform.Translate (0, 0, objectSpeed);
+		}
+	}
+}
